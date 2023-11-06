7c79cb6197b09683a07d68bbe9bcae599c6e6dd1
anuraag0532 <anurag.shrivastava36@gmail.com>
2023-11-06 19:45:57 +0530

    message addition

--- FinancialSystem.java
+++ FinancialSystem.java
@@ -3,40 +3,59 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+class Transaction {
+    String debtor;
+    String creditor;
+    int amount;
+
+    public Transaction(String debtor, String creditor, int amount) {
+        this.debtor = debtor;
+        this.creditor = creditor;
+        this.amount = amount;
+    }
+}
+
 class FinancialSystem {
     private List<Transaction> transactions;
     private Map<String, Integer> debts;
     private Map<String, Integer> moneyOwed;
 
     public FinancialSystem() {
+        // Initialize the data structures to store transactions, debts, and money owed.
         transactions = new ArrayList<>();
         debts = new HashMap<>();
         moneyOwed = new HashMap<>();
     }
 
     public void addTransaction(Transaction transaction) {
+        // Add a new transaction and update the debts and money owed.
         transactions.add(transaction);
         updateDebtsAndMoneyOwed(transaction);
     }
 
     private void updateDebtsAndMoneyOwed(Transaction transaction) {
+        // Update debts and money owed based on the transaction details.
         String debtor = transaction.debtor;
         String creditor = transaction.creditor;
         int amount = transaction.amount;
 
+        // Update the debtor's debt and the creditor's money owed.
         debts.put(debtor, debts.getOrDefault(debtor, 0) + amount);
         moneyOwed.put(creditor, moneyOwed.getOrDefault(creditor, 0) + amount);
     }
 
     public int queryDebt(String person) {
+        // Query the debt of a person; return 0 if not found.
         return debts.getOrDefault(person, 0);
     }
 
     public int queryMoneyOwed(String person) {
+        // Query the money owed to a person; return 0 if not found.
         return moneyOwed.getOrDefault(person, 0);
     }
 
     public String queryPersonWithMostMoneyOwed() {
+        // Find the person with the most money owed and return their name.
         String person = null;
         int maxMoneyOwed = 0;
 
@@ -51,6 +70,7 @@ class FinancialSystem {
     }
 
     public String queryPersonWithMostDebt() {
+        // Find the person with the most debt and return their name.
         String person = null;
         int maxDebt = 0;
 
--- MainSystem.java
+++ MainSystem.java
@@ -10,30 +10,37 @@ public class MainSystem {
             String command = scanner.nextLine();
             String[] parts = command.split(" ");
 
+            // Check the user's command and perform the corresponding action.
             if (parts[0].equals("add_transaction")) {
                 if (parts.length == 4) {
+                    // Parse the input and create a new transaction object.
                     Transaction transaction = new Transaction(parts[1], parts[2], Integer.parseInt(parts[3]));
                     financialSystem.addTransaction(transaction);
                 }
             } else if (parts[0].equals("query_debt")) {
                 if (parts.length == 2) {
+                    // Query and print the debt of a specific person.
                     String person = parts[1];
                     int debt = financialSystem.queryDebt(person);
                     System.out.println(person + " owes a total of " + debt + " money.");
                 }
             } else if (parts[0].equals("query_money_owed")) {
                 if (parts.length == 2) {
+                    // Query and print the money owed to a specific person.
                     String person = parts[1];
                     int moneyOwed = financialSystem.queryMoneyOwed(person);
                     System.out.println(person + " is owed a total of " + moneyOwed + " money.");
                 }
             } else if (parts[0].equals("query_person_with_most_money_owed")) {
+                // Query and print the person owed the most money.
                 String person = financialSystem.queryPersonWithMostMoneyOwed();
                 System.out.println(person + " is owed the most money.");
             } else if (parts[0].equals("query_person_with_most_debt")) {
+                // Query and print the person with the most debt.
                 String person = financialSystem.queryPersonWithMostDebt();
                 System.out.println(person + " owes the most money.");
             } else {
+                // Handle invalid commands and prompt the user to try again.
                 System.out.println("Invalid command. Please try again.");
             }
         }
